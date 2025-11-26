54fcbe237654cbb7f95ce4b3c65b8be56fcb036b
Jo Yu Sung <cys990617@gmail.com>
2025-11-26 20:45:00 +0900

    backend생성

--- init.tf
+++ init.tf
@@ -1,46 +1,45 @@
 terraform {
-     required_providers {
-          aws = {
-             source = "hashicorp/aws"
-             version = "~> 5.17.0"
-          }
-     }
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 5.17.0" # 5.17.x 버전들만 허용
+        }
+    }
 }
 
 provider "aws" {
-     region = "ap-northeast-2"
+    region = "ap-northeast-2"
 }
 
 resource "aws_s3_bucket" "tfstate" {
-  bucket = "inflearn-terraform-${var.account_id}"
+    bucket = "${var.account_id}-apnortheast2-tfstate"
 
-  versioning {
-    enabled = true
-  }
+    versioning {
+        enabled = true # 버전관리 활성화
+    }
 }
 
 resource "aws_dynamodb_table" "terraform_state_lock" {
-  name         = "terraform-lock"
-  hash_key     = "LockID"
-  billing_mode = "PAY_PER_REQUEST"
+    name         = "terraform-lock"
+    hash_key     = "LockID"
+    billing_mode = "PAY_PER_REQUEST"
 
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
-
-variable "account_id" {
-  default = "whdbtjd"
+    attribute {
+        name = "LockID"
+        type = "S"
+    }
 }
 
 terraform {
-  backend "s3" {
-    bucket         = "inflearn-terraform-whdbtjd"   
-    key            = "terraform/terraform.tfstate"   
-    region         = "ap-northeast-2"
-    dynamodb_table = "terraform-lock"                
-    encrypt        = true
-  }
+    backend "s3" {
+        bucket = "whdbtjd-apnortheast2-tfstate"
+        key    = "whdbtjd/terraform/hello"
+        region = "ap-northeast-2"
+        encrypt = true
+        dynamodb_table = "terraform-lock"
+    }
 }
 
+variable "account_id" {
+    default = "whdbtjd"
+}

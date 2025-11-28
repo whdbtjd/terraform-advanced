ad84602b33f3df505523fe5864657020db408578
Yu Seong <cys990617@gmail.com>
2025-11-28 21:47:35 +0900

    서브넷별 라우팅 테이블 경로 설정

--- vpc.tf
+++ vpc.tf
@@ -130,3 +130,16 @@ resource "aws_route_table_association" "private_db" {
     subnet_id      = element(aws_subnet.private_db.*.id, count.index)
     route_table_id = element(aws_route_table.private_db.*.id, count.index)
 }
+
+resource "aws_route" "public_internet_gateway" {
+    route_table_id         = aws_route_table.public.id
+    destination_cidr_block = "0.0.0.0/0"
+    gateway_id             = aws_internet_gateway.default.id
+}
+
+resource "aws_route" "private_nat" {
+  count                  = length(var.availability_zones)
+  route_table_id         = element(aws_route_table.private.*.id, count.index)
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = element(aws_nat_gateway.nat.*.id, count.index)
+}

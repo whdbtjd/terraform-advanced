53499eabe8d0e9271de71246c67dd452edb969e1
Yu Seong <cys990617@gmail.com>
2025-12-01 16:09:54 +0900

    external lb용 태그값 설정

--- _module/var_lb.tf
+++ _module/var_lb.tf
@@ -1,9 +1,17 @@
 variable "lb_variables" {
   default = {
 
-    target_group_slow_start = {}
+    target_group_slow_start = {
+      whdbtjd_1 = 0
+      whdbtjd_2 = 0
+      whdbtjd_3 = 0
+    }
 
-    target_group_deregistration_delay = {}
+    target_group_deregistration_delay = {
+      whdbtjd_1 = 0
+      whdbtjd_2 = 60
+      whdbtjd_3 = 60
+    }
 
     internal_lb = {
       tags = {}
@@ -14,11 +22,59 @@ variable "lb_variables" {
     }
 
     external_lb = {
-      tags = {}
+      tags = {
+        whdbtjd_1 = {
+          Name    = "hello-whdbtd_1-external-lb"
+          app     = "hello"
+          project = "hello"
+          env     = "dev"
+          stack   = "whdbtd_1"
+        },
+
+        whdbtjd_2 = {
+          Name    = "hello-whdbtjd_2-external-lb"
+          app     = "hello"
+          project = "hello"
+          env     = "dev"
+          stack   = "whdbtjd_2"
+        },
+
+        whdbtjd_3 = {
+          Name    = "hello-whdbtjd_3-external-lb"
+          app     = "hello"
+          project = "hello"
+          env     = "prod"
+          stack   = "whdbtjd_3"
+        }
+      }
     }
 
     external_lb_tg = {
-      tags = {}
+      tags = {
+        whdbtjd_1 = {
+          Name    = "hello-whdbtd_1-external-tg"
+          app     = "hello"
+          project = "hello"
+          env     = "dev"
+          stack   = "whdbtd_1"
+        },
+
+        whdbtjd_2 = {
+          Name    = "hello-whdbtjd_2-external-tg"
+          app     = "hello"
+          project = "hello"
+          env     = "dev"
+          stack   = "whdbtjd_2"
+        },
+
+        whdbtjd_3 = {
+          Name    = "hello-whdbtjd_3-external-tg"
+          app     = "hello"
+          project = "hello"
+          env     = "prod"
+          stack   = "whdbtjd_3"
+        }
+      }
     }
   }
 }

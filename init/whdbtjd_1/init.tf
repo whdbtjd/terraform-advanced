af5fb5f04530031296d33dfda5c4fe1682dce5d0
Yu Seong <cys990617@gmail.com>
2025-12-01 21:16:22 +0900

    환경 분리 진행 중

---
+++ init/whdbtjd_1/init.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.17.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "ap-northeast-2"
+}
+
+# S3 bucket for backend
+resource "aws_s3_bucket" "tfstate" {
+  bucket = "${var.account_id}-apnortheast2-tfstate"
+
+  versioning {
+    enabled = true # Prevent from deleting tfstate file
+  }
+}
+
+# DynamoDB for terraform state lock
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  name           = "terraform-lock"
+  hash_key       = "LockID"
+  billing_mode   = "PAY_PER_REQUEST"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+variable "account_id" {
+  default = "whdbtjd-apnortheast2"
+}
---
+++ vpc/whdbtjd_1/bastion_sg.tf
@@ -0,0 +1,65 @@
+resource "aws_security_group" "bastion" {
+    name = "bastion-${var.vpc_name}"
+    description = "Allows SSH access to the bastion server"
+
+    vpc_id = aws_vpc.default.id
+
+    ingress {
+        from_port = 22
+        to_port   = 22
+        protocol  = "tcp"
+        
+        cidr_blocks= ["0.0.0.0/0"]
+    }
+    egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "http port any outbound"
+  }
+
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "https port any outbound"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "bastion-${var.vpc_name}"
+  }
+}
+
+resource "aws_security_group" "bastion_aware" {
+    name = "bastion_aware_${var.vpc_name}"
+    description = "Allows SSH access from the Bastion server"
+
+    vpc_id = aws_vpc.default.id
+
+    ingress {
+        from_port = 22
+        to_port   = 22
+        protocol  = "tcp"
+        security_groups = [aws_security_group.bastion.id]
+    }
+
+    egress {
+        from_port = 0
+        to_port   = 0
+        protocol  = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "bastionAware-${var.vpc_name}"
+    }
+}

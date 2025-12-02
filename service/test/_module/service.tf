242d2a3648a532421e412cab2f31d70bd975b154
Yu Seong <cys990617@gmail.com>
2025-12-02 15:24:27 +0900

    asg 생성

--- service/test/_module/service.tf
+++ service/test/_module/service.tf
@@ -134,3 +134,42 @@ resource "aws_lb_listener" "external_80" {
         }
     }
 }
+
+#### ASG
+##### ASG
+
+resource "aws_launch_template" "lt" {
+  name_prefix             = "${var.service_name}-${var.vpc_name}-LT"
+  image_id                = var.image_id
+  instance_type           = var.instance_type
+  key_name                = var.key_name
+
+  vpc_security_group_ids  = [aws_security_group.ec2.id]
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "${var.service_name}-${var.vpc_name}-LaunchTemplate"
+    }
+  }
+}
+
+resource "aws_autoscaling_group" "asg" {
+  launch_template {
+    id      = aws_launch_template.lt.id
+    version = "$Latest"
+  }
+
+  min_size            = var.min_size
+  max_size            = var.max_size
+  desired_capacity    = var.desired_capacity
+  vpc_zone_identifier = var.private_subnets
+
+  target_group_arns = [aws_lb_target_group.internal.arn]
+
+  tag {
+    key                  = "Name"
+    value                = "${var.service_name}-${var.vpc_name}-asg"
+    propagate_at_launch  = true
+  }
+}
